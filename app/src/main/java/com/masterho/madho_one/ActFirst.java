c620c1427d10d34b80c127f61db3726b31f3d1b2
MasterHo1228 <masterho1228@gmail.com>
2016-07-26 23:37:07 +0800

    Simplified Java code and update gradle build tools version.

--- app/src/main/java/com/masterho/madho_one/ActFirst.java
+++ app/src/main/java/com/masterho/madho_one/ActFirst.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class ActFirst extends AppCompatActivity {
+public class ActFirst extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,30 +14,30 @@ public class ActFirst extends AppCompatActivity {
         setContentView(R.layout.activity_act_first);
 
         Button btnAct1 = (Button) findViewById(R.id.btnAct1);
-        btnAct1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent Act1 = new Intent(ActFirst.this,Act1.class);
-                startActivity(Act1);
-            }
-        });
+        btnAct1.setOnClickListener(this);
 
         Button btnAct2 = (Button) findViewById(R.id.btnAct2);
-        btnAct2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent Act2 = new Intent(ActFirst.this,Act2.class);
-                startActivity(Act2);
-            }
-        });
+        btnAct2.setOnClickListener(this);
 
         Button btnAct3 = (Button) findViewById(R.id.btnAct3);
-        btnAct3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        btnAct3.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnAct1:
+                Intent Act1 = new Intent(ActFirst.this,Act1.class);
+                startActivity(Act1);
+                break;
+            case R.id.btnAct2:
+                Intent Act2 = new Intent(ActFirst.this,Act2.class);
+                startActivity(Act2);
+                break;
+            case R.id.btnAct3:
                 Intent Act3 = new Intent(ActFirst.this,Act3.class);
                 startActivity(Act3);
-            }
-        });
+                break;
+        }
     }
 }
